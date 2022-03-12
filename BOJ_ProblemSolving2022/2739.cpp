1e2b9715bb135f5a143ed873a31b490fe8805cb6
Harin <harin608@naver.com>
2022-03-13 02:57:52 +0900

    2739. 구구단

---
+++ BOJ_ProblemSolving2022/2739.cpp
@@ -0,0 +1,12 @@
+//Q.2739 ±¸±¸´Ü
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= 9; i++) {
+		cout << n << " * " << i << " = " << n * i << '\n';
+	}
+}
