1214f64a63b20967b4e984a4cf4026e578708a3f
Harin <harin608@naver.com>
2022-03-14 13:13:10 +0900

    2442. 별찍기-5

---
+++ BOJ_ProblemSolving2022/2442.cpp
@@ -0,0 +1,20 @@
+//Q2442. º°Âï±â-5
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int k = 1; k <= n - i; k++)
+			cout << ' ';
+		for (int j = 1; j <= 2 * i - 1; j++)
+			cout << '*';
+		cout << '\n';
+	}
+
+}
