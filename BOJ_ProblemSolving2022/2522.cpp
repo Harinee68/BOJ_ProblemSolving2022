3279ba56508ba8e41b05c93e8b25d8aeab0b4126
Harin <harin608@naver.com>
2022-03-14 15:30:59 +0900

    2522. 별찍기-12

---
+++ BOJ_ProblemSolving2022/2522.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = n - i; j >= 1; j--)
+			cout << ' ';
+		for (int k = 1; k <= i; k++)
+			cout << '*';
+		cout << '\n';
+	}
+
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = 1; j <= i; j++)
+			cout << ' ';
+		for (int k = n - i; k >= 1; k--)
+			cout << '*';
+		cout << '\n';
+	}
+
+	return 0;
+}
